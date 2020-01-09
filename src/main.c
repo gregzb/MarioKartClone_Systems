dd7cd4cf441fe2d7843700507fef25f6623fa716
Greg Zborovsky <greg.zbo3@gmail.com>
2020-01-08 20:31:10 -0500

    moved files, slightly edited makefile to be more portable

--- src/main.c
+++ src/main.c
@@ -47,7 +47,7 @@ int main(int argc, char *args[])
 	if (!(init_sdl() && init_window() && init_renderer()))
 		return -1;
 
-	bg_image = load_image("test_image.bmp");
+	bg_image = load_image("resources/images/test_image.bmp");
 
 	clock_gettime(CLOCK_MONOTONIC, &init_time);
 	last_time = init_time;
