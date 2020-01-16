f6fa23cb64b44662f5a8ed35b8609122207a0581
Emma Choi <emmachoi@Emmas-MacBook-Air-3.local>
2020-01-16 10:34:17 -0500

    added reverse dir

--- src/kart.h
+++ src/kart.h
@@ -19,6 +19,7 @@ void kart_move(struct kart * current_kart, char acc, char lr, double dt);
 void kart_update_direction(struct kart * current_kart, char lr, double dt);
 void kart_update_position(struct kart * current_kart);
 void kart_update_velocity(struct kart * current_kart);
+void kart_reverse_direction(struct kart * current_kart);
 // struct kart update_acceleration(struct kart current_kart, int xdir, int ydir);//negative number for down, left, position for up, right, 0 for neither
 // int add_acceleration(double currentA, double addition, int dir);
 
